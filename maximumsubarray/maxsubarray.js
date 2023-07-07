a04690e5faab05f580e71b4522b39f22be39e258
Daniel Yoon <64057788+yd24@users.noreply.github.com>
2023-07-06 21:17:38 -0700

    Implement Kadane's Algorithm and related unit tests, add whiteboard references

--- maximumsubarray/maxsubarray.js
+++ maximumsubarray/maxsubarray.js
@@ -1,16 +1,15 @@
 function maxSumSubArray(nums) {
-  let maxSum = nums[0];
-  let sum;
-  for (let i = 0;  i < nums.length; i++) {
-    sum = 0;
-    for (let j = i; j < nums.length; j++) {
-      sum += nums[j];
-      if (sum > maxSum) {
-        maxSum = sum;
+  let globalMax = nums[0];
+  let localMax = nums[0];
+  if (nums.length > 1) {
+    for (let i = 1;  i < nums.length; i++) {
+      localMax = Math.max(localMax + nums[i], nums[i]);
+      if (localMax > globalMax) {
+        globalMax = localMax;
       }
     }
   }
-  return maxSum;
+  return globalMax;
 }
 
 module.exports = maxSumSubArray;
