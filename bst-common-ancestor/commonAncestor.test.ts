33d1269396133d820cc5e48e026f616692d75be6
Daniel Yoon <64057788+yd24@users.noreply.github.com>
2023-07-18 11:56:08 -0700

    Implement solution and related unit test for finding lowest common ancestor

--- bst-common-ancestor/commonAncestor.test.ts
+++ bst-common-ancestor/commonAncestor.test.ts
@@ -1,5 +1,6 @@
 import { describe, test, expect } from 'vitest';
 import { BinarySearchTree } from './bst';
+import { commonAncestor } from './commonAncestor';
 
 describe('Testing function to find common ancestor in binary search tree', () => {
   test('Can create a binary search tree and add elements to it', () => {
@@ -21,5 +22,21 @@ describe('Testing function to find common ancestor in binary search tree', () =>
     expect(order).toEqual([0, 2, 3, 4, 5, 6, 7, 8, 9]);
     expect(post).toEqual([0, 3, 5, 4, 2, 7, 9, 8, 6]);
     expect(pre).toEqual([6, 2, 0, 4, 3, 5, 8, 7, 9]);
+  });
+
+  test('Can find the lowest common ancestor in regular BST', () => {
+    let bst = new BinarySearchTree();
+    bst.add(6);
+    bst.add(2);
+    bst.add(8);
+    bst.add(0);
+    bst.add(4);
+    bst.add(7);
+    bst.add(9);
+    bst.add(3);
+    bst.add(5);
+
+    let LCA = commonAncestor(bst.root, 2, 8);
+    expect(LCA.value).toEqual(6);
   });
 });
--- bst-common-ancestor/commonAncestor.ts
+++ bst-common-ancestor/commonAncestor.ts
@@ -1,7 +1,29 @@
 import {TreeNode} from './bst';
 
-export function commonAncestor(root: TreeNode): number {
-  
-  
-  return 0;
+export function commonAncestor(root: TreeNode, p: TreeNode, q: TreeNode): number {
+  let current = root;
+  while (current) {
+    if (p.value > current.value && q.value > current.value) {
+      current = current.right;
+    } else if (p.value < current.value && q.value < current.value) {
+      current = current.left;
+    } else {
+      return current.value;
+    }
+  }
+}
+
+export function commonAncestorRecursive(root: TreeNode, p: TreeNode, q: TreeNode): number {
+  let commonAncestor = commonAncestorRecursHelper(root, p, q);
+  return commonAncestor.value;
+}
+
+export function commonAncestorRecursHelper(current: TreeNode, p: TreeNode, q: TreeNode): TreeNode {
+  if (p.value > current.value && q.value > current.value) {
+    return commonAncestorRecursHelper(current.right, p, q);
+  } else if (p.value < current.value && q.value < current.value) {
+    return commonAncestorRecursHelper(current.left, p, q);
+  } else {
+    return current;
+  }
 }
